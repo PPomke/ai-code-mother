2d9f216580153fc2b8ae8ff5c05e3f87dcc6f9c9
griffin.wang <griffin.wang@washiai.com>
2026-02-25 17:13:54 +0800

    基础应用代码

---
+++ src/main/java/com/griffin/aicodemother/mapper/AppMapper.java
@@ -0,0 +1,14 @@
+package com.griffin.aicodemother.mapper;
+
+import com.griffin.aicodemother.model.entity.App;
+import com.mybatisflex.core.BaseMapper;
+
+
+/**
+ * 应用 映射层。
+ *
+ * @author griffin
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/griffin/aicodemother/service/AppService.java
@@ -0,0 +1,24 @@
+package com.griffin.aicodemother.service;
+
+import com.griffin.aicodemother.model.dto.app.AppQueryRequest;
+import com.griffin.aicodemother.model.entity.App;
+import com.griffin.aicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author griffin
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+}
